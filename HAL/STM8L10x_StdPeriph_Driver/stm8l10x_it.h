71c5b6cd3c33c3df54e608de2ae2ec448cf7371f
gabbla <zampierigabriele.ist@gmail.com>
2021-12-20 16:52:14 +0100

    hal: added sdcc interrupt handler def

--- HAL/STM8L10x_StdPeriph_Driver/stm8l10x_it.h
+++ HAL/STM8L10x_StdPeriph_Driver/stm8l10x_it.h
@@ -41,7 +41,8 @@
  INTERRUPT void NonHandledInterrupt(void);
 #endif /* _COSMIC_ */
 
-#ifndef _RAISONANCE_
+// SDCC patch: requires separate handling for SDCC (see below)
+#if !defined(_RAISONANCE_) && !defined(_SDCC_)
  INTERRUPT void NonHandledInterrupt(void);
  INTERRUPT void TRAP_IRQHandler(void); /* TRAP */
  INTERRUPT void FLASH_IRQHandler(void); /* FLASH EOP/PG_DIS */
@@ -66,7 +67,36 @@
  INTERRUPT void USART_TX_IRQHandler(void); /* USART TX */
  INTERRUPT void USART_RX_IRQHandler(void); /* USART RX */
  INTERRUPT void I2C_IRQHandler(void); /* I2C */
-#endif /* _COSMIC_ */
+
+
+// SDCC patch: __interrupt keyword required after function name --> requires new block
+#elif defined (_SDCC_)
+
+ INTERRUPT_HANDLER_TRAP(TRAP_IRQHandler);                /* TRAP */
+ INTERRUPT_HANDLER(FLASH_IRQHandler, 1);                 /* FLASH EOP/PG_DIS */
+ INTERRUPT_HANDLER(AWU_IRQHandler,   4);                 /* AWU */
+ INTERRUPT_HANDLER(EXTIB_IRQHandler, 6);                 /* EXTI PORTB */
+ INTERRUPT_HANDLER(EXTID_IRQHandler, 7);                 /* EXTI PORTD */
+ INTERRUPT_HANDLER(EXTI0_IRQHandler, 8);                 /* EXTI PIN0 */
+ INTERRUPT_HANDLER(EXTI1_IRQHandler, 9);                 /* EXTI PIN1 */
+ INTERRUPT_HANDLER(EXTI2_IRQHandler, 10);                /* EXTI PIN2 */
+ INTERRUPT_HANDLER(EXTI3_IRQHandler, 11);                /* EXTI PIN3 */
+ INTERRUPT_HANDLER(EXTI4_IRQHandler, 12);                /* EXTI PIN4 */
+ INTERRUPT_HANDLER(EXTI5_IRQHandler, 13);                /* EXTI PIN5 */
+ INTERRUPT_HANDLER(EXTI6_IRQHandler, 14);                /* EXTI PIN6 */
+ INTERRUPT_HANDLER(EXTI7_IRQHandler, 15);                /* EXTI PIN7 */
+ INTERRUPT_HANDLER(COMP_IRQHandler, 18);                 /* COMP */
+ INTERRUPT_HANDLER(TIM2_UPD_OVF_TRG_BRK_IRQHandler, 19); /* TIM2 UPD/OVF/TRG/BRK */
+ INTERRUPT_HANDLER(TIM2_CAP_IRQHandler, 20);             /* TIM2 CAP*/
+ INTERRUPT_HANDLER(TIM3_UPD_OVF_TRG_BRK_IRQHandler, 21); /* TIM3 UPD/OVF/TRG/BRK */
+ INTERRUPT_HANDLER(TIM3_CAP_IRQHandler, 22);             /* TIM3 CAP*/
+ INTERRUPT_HANDLER(TIM4_UPD_OVF_IRQHandler, 25);         /* TIM4 UPD/OVF */
+ INTERRUPT_HANDLER(SPI_IRQHandler, 26);                  /* SPI */
+ INTERRUPT_HANDLER(USART_TX_IRQHandler, 27);             /* USART TX */
+ INTERRUPT_HANDLER(USART_RX_IRQHandler, 28);             /* USART RX */
+ INTERRUPT_HANDLER(I2C_IRQHandler, 29);                  /* I2C */
+
+#endif /* !(_RAISONANCE_) && !(_SDCC_) */
 
 #endif /* __STM8L10x_IT_H */
 
